c28ce06eb3d0e2a4e03560c8a6c44375ba42da59
EliGramuglia <eliana.gramuglia@gmail.com>
2023-10-12 10:01:20 -0300

    Se modificaron varios archivos

---
+++ app/models/jugadores.model.php
@@ -0,0 +1,22 @@
+<?php
+
+class JugadoresModel{
+    private $db;
+
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;dbname=db_jugadores_club;charset=utf8', 'root', '');
+
+    }
+
+
+
+    public function getJugadores(){
+        $query = $this->db->prepare('SELECT * FROM jugadores');
+        
+        $query->execute();
+
+        $jugadores = $query->fetchall(PDO::FETCH_OBJ);
+
+        return $jugadores;
+    }
+}
